aacec6c1cf95dc3b03c0c9888cbb3c16ce296813
Dmitriy <belo.dima@gmail.com>
2018-03-06 16:29:06 +0500

    Bubble sort

---
+++ BubbleSort.php
@@ -0,0 +1,32 @@
+<?php
+declare(strict_types=1);
+
+namespace Patterns\Strategy;
+
+class BubbleSort
+{
+	public function sort(array &$array) : void
+	{
+		$size = count($array);
+		for($i = 0; $i < $size; $i++){
+
+			$transposition = false;
+
+			for($j = 0; $j < $size - $i - 1; $j++){
+
+				if($array[$j] > $array[$j + 1]){
+
+					$tmp = $array[$j];
+					$array[$j] = $array[$j + 1];
+					$array[$j + 1] = $tmp;
+					$transposition = true;
+				}
+			}
+
+			if(!$transposition){
+				break;
+			}
+
+		}
+	}
+}
